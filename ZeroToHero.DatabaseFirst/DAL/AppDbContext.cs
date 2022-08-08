5b54e1f6d2d969fded0040da1bf58b99316c43f3
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-08-08 11:12:16 +0300

    added dbContext and dbSet

---
+++ ZeroToHero.DatabaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ZeroToHero.DatabaseFirst.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-M7H0KCS;Initial Catalog=ZeroToHeroDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ ZeroToHero.DatabaseFirst/DAL/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ZeroToHero.DatabaseFirst.DAL
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
