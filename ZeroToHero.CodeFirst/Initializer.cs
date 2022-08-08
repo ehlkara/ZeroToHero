9370d9ac5d8371c2e54aa63628730a987e20919e
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-08-08 14:04:39 +0300

    created codefirst project

---
+++ ZeroToHero.CodeFirst/Initializer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ZeroToHero.CodeFirst
+{
+    public class Initializer
+    {
+        public static IConfigurationRoot Configuration;
+
+        public static void Build()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+    }
+}
---
+++ ZeroToHero.CodeFirst/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.EntityFrameworkCore;
+using ZeroToHero.CodeFirst;
+using ZeroToHero.CodeFirst.DAL;
+
+Initializer.Build();
+using(var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id}: {p.Name} - {p.Price} - {p.Stock}");
+    });
+}
