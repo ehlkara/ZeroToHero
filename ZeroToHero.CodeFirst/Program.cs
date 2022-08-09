721b8127035c857a79e60169777fbc71f481ed3b
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-08-09 13:08:07 +0300

    States notion

--- ZeroToHero.CodeFirst/Program.cs
+++ ZeroToHero.CodeFirst/Program.cs
@@ -7,10 +7,41 @@ using ZeroToHero.CodeFirst.DAL;
 Initializer.Build();
 using(var _context = new AppDbContext())
 {
-    var products = await _context.Products.ToListAsync();
+    //var products = await _context.Products.ToListAsync();
 
-    products.ForEach(p =>
-    {
-        Console.WriteLine($"{p.Id}: {p.Name} - {p.Price} - {p.Stock}");
-    });
+    //products.ForEach(p =>
+    //{
+    //    var state = _context.Entry(p).State;
+
+    //    Console.WriteLine($"{p.Id}: {p.Name} - {p.Price} - {p.Stock} state: {state}");
+    //});
+
+    //var newProduct = new Product { Name = "Pen 300", Price = 200, Stock = 100, Barcode = 333 };
+
+    //var product = await _context.Products.FirstAsync();
+
+    _context.Update(new Product() { Id = 5, Name = "Notebook", Price = 500, Stock = 500, Barcode = 500 });
+
+    //Console.WriteLine($"First State: {_context.Entry(product).State}");
+
+    //_context.Entry(product).State = EntityState.Detached;
+
+    //Console.WriteLine($"Last State: {_context.Entry(product).State}");
+
+    //product.Stock = 1000;
+
+    //product.Name = "Pen 2000";
+
+    //_context.Remove(product);
+    //_context.Entry(product).State = EntityState.Deleted;
+
+    //_context.Entry(newProduct).State = EntityState.Added;
+
+    //await _context.AddAsync(newProduct);
+
+
+
+    await _context.SaveChangesAsync();
+
+    //Console.WriteLine($"State after save changes: {_context.Entry(product).State}");
 }
