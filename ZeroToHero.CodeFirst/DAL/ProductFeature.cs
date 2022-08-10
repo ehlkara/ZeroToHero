04981a5f2a340c9c1633bae5c5161ba9ff97c12a
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-08-10 17:43:54 +0300

    attribute way one to one relationships

--- ZeroToHero.CodeFirst/DAL/ProductFeature.cs
+++ ZeroToHero.CodeFirst/DAL/ProductFeature.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace ZeroToHero.CodeFirst.DAL
         public int Height { get; set; }
         public string Color { get; set; }
         public int ProductRef_Id { get; set; }
+        [ForeignKey("ProductRef_Id")]
         public Product Product { get; set; }
     }
 }
