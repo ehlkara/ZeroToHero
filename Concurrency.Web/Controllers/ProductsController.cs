de0fbba441e1bff9aba1a55f77a007fa44d4a7d0
ehlkara <ehlullah.fb@gmail.com>
2022-09-06 09:47:07 +0300

    Update productsController

--- Concurrency.Web/Controllers/ProductsController.cs
+++ Concurrency.Web/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Concurrency.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +11,19 @@ namespace Concurrency.Web.Controllers
 {
     public class ProductsController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Update(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            return View(product);
         }
     }
 }
