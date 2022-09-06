ac1c1ae0ad6aa04cc6d82deba8bf3e816a108070
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-09-06 10:45:22 +0300

    added DbUpdateConcurrencyExceptionException and finished project

--- Concurrency.Web/Controllers/ProductsController.cs
+++ Concurrency.Web/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Concurrency.Web.Models;
@@ -34,10 +35,35 @@ namespace Concurrency.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Update(Product product)
         {
-            _context.Products.Update(product);
-            await _context.SaveChangesAsync();
+            try
+            {
+                _context.Products.Update(product);
+                await _context.SaveChangesAsync();
 
-            return RedirectToAction(nameof(List));
+                return RedirectToAction(nameof(List));
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                var exceptionEntry = exception.Entries.First();
+                var currentProduct = exceptionEntry.Entity as Product;
+                var databaseValues = exceptionEntry.GetDatabaseValues();
+                
+                var clientValues = exceptionEntry.CurrentValues;
+
+                if(databaseValues == null)
+                {
+                    ModelState.AddModelError(string.Empty, "This product deleted from another user");
+                }
+                else
+                {
+                    var databaseProduct = databaseValues.ToObject() as Product;
+                    ModelState.AddModelError(string.Empty, "This product updated from another user");
+                    ModelState.AddModelError(string.Empty, $"Updated Values: Name:{databaseProduct.Name}, Price:" +
+                        $"{databaseProduct.Price}, Stock:{databaseProduct.Stock}");
+                }
+
+                return View(product);
+            }
         }
     }
 }
