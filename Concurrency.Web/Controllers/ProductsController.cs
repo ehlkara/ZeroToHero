d668b7ecc07f3c6e2ab795b6ddfc5c43ceb3f3ad
Ehlullah Karakurt <52125689+ehlkara@users.noreply.github.com>
2022-09-06 10:20:08 +0300

    created product update page

--- Concurrency.Web/Controllers/ProductsController.cs
+++ Concurrency.Web/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Concurrency.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,12 +19,25 @@ namespace Concurrency.Web.Controllers
             _context = context;
         }
 
-        // GET: /<controller>/
+        public async Task<IActionResult> List()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
         public async Task<IActionResult> Update(int id)
         {
             var product = await _context.Products.FindAsync(id);
 
             return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(List));
         }
     }
 }
